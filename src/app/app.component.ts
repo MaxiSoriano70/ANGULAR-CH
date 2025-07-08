a0e5a8ec93836ae0ccfe02e0a33a90ab2da22d2a
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-07-08 02:17:00 -0300

    Clase 1 Primeros pasops en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentComponent } from "./student/student.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [StudentComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
