d987c07a85a1d9b3b67f589858401029d6022eb4
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-07-15 05:54:11 -0300

    Clase 3 FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,17 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { PerfilUsuarioComponent } from "./perfil-usuario/perfil-usuario.component";
 import { HijoComponent } from './hijo/hijo.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [StudentComponent, NavbarComponent, CommonModule, PerfilUsuarioComponent, HijoComponent],
+  imports: [ReactiveFormsModule, StudentComponent, NavbarComponent, CommonModule, PerfilUsuarioComponent, HijoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  form: FormGroup;
+
   @ViewChild("btnMain") btnMain!:ElementRef<HTMLButtonElement>;
 
   name : string = "celeste";
@@ -23,17 +26,29 @@ export class AppComponent {
   namesArray = ["Celeste", "Melissa", "Tati"];
   myDate = new Date();
 
-  ngAfterViewIbit(){
-    console.log("btnMain", this.btnMain.nativeElement);
-    this.btnMain.nativeElement.blur();
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      nombre: ["", [Validators.required, Validators.minLength(3)]]
+    });
   }
 
+  /* DUDA */
+  /*ngAfterViewInit(){
+    console.log("btnMain", this.btnMain.nativeElement);
+    this.btnMain.nativeElement.blur();
+  }*/
+
   ngOnInit(){
-    setTimeout(()=>{
-      this.loading = false;
-      this.name = "CELESTE";
-    }, 10000);
-  }
+  setTimeout(() => {
+    this.loading = false;
+    this.name = "CELESTE";
+
+    setTimeout(() => {
+      this.btnMain?.nativeElement.blur();
+    });
+  }, 10000);
+}
+
 
   nameChange(newName : string){
     this.name = newName;
